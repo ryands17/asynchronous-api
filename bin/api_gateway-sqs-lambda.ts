5288465650f55aa44695f1039ec1aba28e940a9a
Ryan <dsouza.ryan.11@gmail.com>
2020-09-24 15:48:49 +0530

    initial commit

--- bin/api_gateway-sqs-lambda.ts
+++ bin/api_gateway-sqs-lambda.ts
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { ApiGatewaySqsLambdaStack } from '../lib/api_gateway-sqs-lambda-stack';
+import 'source-map-support/register'
+import * as cdk from '@aws-cdk/core'
+import { ApiGatewaySqsLambdaStack } from '../lib/api_gateway-sqs-lambda-stack'
 
-const app = new cdk.App();
-new ApiGatewaySqsLambdaStack(app, 'ApiGatewaySqsLambdaStack');
+const REGION = process.env.REGION || 'us-east-1'
+
+const app = new cdk.App()
+new ApiGatewaySqsLambdaStack(app, 'ApiGatewaySqsLambdaStack', {
+  env: { region: REGION },
+})
