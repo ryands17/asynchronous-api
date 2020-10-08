604171ba377b94c959d618446307c4b77a951d00
Ryan <dsouza.ryan.11@gmail.com>
2020-10-09 00:22:30 +0530

    add env variables validation

--- bin/api_gateway-sqs-lambda.ts
+++ bin/api_gateway-sqs-lambda.ts
@@ -3,10 +3,20 @@ import 'source-map-support/register'
 import * as cdk from '@aws-cdk/core'
 import { ApiGatewaySqsLambdaStack } from '../lib/api_gateway-sqs-lambda-stack'
 
-const REGION = process.env.REGION || 'us-east-1'
-const ACCOUNT_ID = process.env.AWS_ACCOUNT_ID
+const envVars = {
+  REGION: process.env.REGION || 'us-east-1',
+  ACCOUNT_ID: process.env.AWS_ACCOUNT_ID,
+}
 
+validateEnvVariables()
 const app = new cdk.App()
 new ApiGatewaySqsLambdaStack(app, 'ApiGatewaySqsLambdaStack', {
-  env: { region: REGION, account: ACCOUNT_ID },
+  env: { region: envVars.REGION, account: envVars.ACCOUNT_ID },
 })
+
+export function validateEnvVariables() {
+  for (let variable in envVars) {
+    if (!envVars[variable as keyof typeof envVars])
+      throw Error(`Environment variable ${variable} is not defined!`)
+  }
+}
