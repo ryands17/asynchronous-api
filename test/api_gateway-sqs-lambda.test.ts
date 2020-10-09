0689023fb37ad0490d35257f0f6b78efec91b460
Ryan <dsouza.ryan.11@gmail.com>
2020-10-09 20:53:41 +0530

    add tests and dependency management

--- test/api_gateway-sqs-lambda.test.ts
+++ test/api_gateway-sqs-lambda.test.ts
@@ -1,13 +1,142 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as ApiGatewaySqsLambda from '../lib/api_gateway-sqs-lambda-stack';
+import { expect as expectCDK, haveResourceLike } from '@aws-cdk/assert'
+import * as cdk from '@aws-cdk/core'
+import * as ApiGatewaySqsLambda from '../lib/api_gateway-sqs-lambda-stack'
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new ApiGatewaySqsLambda.ApiGatewaySqsLambdaStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
-});
+test('SQS Queue, Lambda Handler, and the corresponding Event Source', () => {
+  const stack = createStack()
+  expectCDK(stack).to(
+    haveResourceLike('AWS::SQS::Queue', {
+      MessageRetentionPeriod: 172800,
+      QueueName: 'async-requests',
+      ReceiveMessageWaitTimeSeconds: 20,
+    })
+  )
+
+  expectCDK(stack).to(
+    haveResourceLike('AWS::Lambda::Function', {
+      Handler: 'index.handler',
+      Runtime: 'nodejs12.x',
+      Environment: {
+        Variables: {
+          QUEUE_URL: {},
+        },
+      },
+      MemorySize: 256,
+      ReservedConcurrentExecutions: 20,
+      Timeout: 5,
+    })
+  )
+
+  expectCDK(stack).to(
+    haveResourceLike('AWS::Lambda::EventSourceMapping', {
+      EventSourceArn: {},
+      FunctionName: {},
+      BatchSize: 1,
+    })
+  )
+})
+
+test('API Gateway and its corresponding IAM Role', () => {
+  const stack = createStack()
+  expectCDK(stack).to(
+    haveResourceLike('AWS::ApiGateway::RestApi', {
+      EndpointConfiguration: {
+        Types: ['REGIONAL'],
+      },
+      Name: 'async-api',
+    })
+  )
+
+  expectCDK(stack).to(
+    haveResourceLike('AWS::IAM::Role', {
+      AssumeRolePolicyDocument: {
+        Statement: [
+          {
+            Action: 'sts:AssumeRole',
+            Effect: 'Allow',
+            Principal: {
+              Service: 'apigateway.amazonaws.com',
+            },
+          },
+        ],
+        Version: '2012-10-17',
+      },
+    })
+  )
+})
+
+test(`API Gateway 'POST' Method and its corresponding Model (Schema)`, () => {
+  const stack = createStack()
+  expectCDK(stack).to(
+    haveResourceLike('AWS::ApiGateway::Method', {
+      HttpMethod: 'POST',
+      AuthorizationType: 'NONE',
+      Integration: {
+        IntegrationHttpMethod: 'POST',
+        IntegrationResponses: [
+          {
+            ResponseTemplates: {
+              'application/json': '{"success":true}',
+            },
+            StatusCode: '200',
+          },
+          {
+            ResponseTemplates: {
+              'application/json': '{"success":false,"error":"Server Error!"}',
+            },
+            SelectionPattern: '500',
+            StatusCode: '500',
+          },
+        ],
+        PassthroughBehavior: 'NEVER',
+        RequestParameters: {
+          'integration.request.header.Content-Type':
+            "'application/x-www-form-urlencoded'",
+        },
+        Type: 'AWS',
+      },
+      MethodResponses: [
+        {
+          ResponseParameters: {
+            'method.response.header.Content-Type': true,
+          },
+          StatusCode: '200',
+        },
+        {
+          ResponseParameters: {
+            'method.response.header.Content-Type': true,
+          },
+          StatusCode: '500',
+        },
+      ],
+      RequestModels: {
+        'application/json': {},
+      },
+    })
+  )
+
+  expectCDK(stack).to(
+    haveResourceLike('AWS::ApiGateway::Model', {
+      ContentType: 'application/json',
+      Schema: {
+        $schema: 'http://json-schema.org/draft-04/schema#',
+        title: 'SQS Payload',
+        type: 'object',
+        required: ['data'],
+        properties: {
+          data: {
+            type: 'string',
+            minLength: 1,
+          },
+        },
+      },
+    })
+  )
+})
+
+function createStack() {
+  const app = new cdk.App()
+  return new ApiGatewaySqsLambda.ApiGatewaySqsLambdaStack(app, 'AsyncAPI', {
+    env: { region: process.env.REGION, account: process.env.AWS_ACCOUNT },
+  })
+}
